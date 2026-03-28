f75d8087afe81fff5e241565d4d93a1c3fd8cc98
aksworns22 <wkdeogks17@gmail.com>
2026-03-29 01:04:01 +0900

    refactor: KanbanProject의 tasks 필드 접근 제어자를 private으로 변경

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/model/KanbanProject.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/model/KanbanProject.kt
@@ -1,6 +1,6 @@
 package woowacourse.kanban.board.domain.model
 
-data class KanbanProject(val name: String, val tasks: List<Task>) {
+data class KanbanProject(val name: String, private val tasks: List<Task>) {
     val totalCount: Int get() = tasks.size
     val completeCount: Int get() = tasks.count { it.status == Status.DONE }
     val completeRatio: Float get() = if (totalCount == 0) 0f else completeCount.toFloat() / totalCount.toFloat()
