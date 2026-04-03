c323c11d9ed2f1a7fb14a65ec11c7c85204aa0be
aksworns22 <wkdeogks17@gmail.com>
2026-04-03 20:33:22 +0900

    test: KanbanProjectState 태스크 삭제 및 상태 관리 테스트 추가 및 리팩터링

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/board/KanbanProjectTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/board/KanbanProjectTest.kt
@@ -10,30 +10,10 @@ import woowacourse.kanban.board.domain.model.Task
 import woowacourse.kanban.board.domain.model.User
 
 class KanbanProjectTest {
-    // given to-do, in-progress, done 상태의 태스크를 각각 1개씩 가진 프로젝트가 주어진다
-    private lateinit var project: KanbanProjectState
-    private lateinit var toDoTask: Task
-    private lateinit var inProgressTask: Task
-    private lateinit var doneTask: Task
-
-    @Before
-    fun setUp() {
-        toDoTask =
-            Task(title = "우리땅!", tags = Tags(emptyList()), status = Status.TODO, user = User.Assignee("두릅"))
-        inProgressTask = Task(
-            title = "우리땅!",
-            tags = Tags(emptyList()),
-            status = Status.IN_PROGRESS,
-            user = User.Assignee("부릅"),
-        )
-        doneTask =
-            Task(title = "우리땅!", tags = Tags(emptyList()), status = Status.DONE, user = User.Assignee("꽈뚜릅"))
-        project = KanbanProjectState(name = "독도는 우리땅", toDoTask, inProgressTask, doneTask)
-    }
-
     @Test
     fun `새로운 태스크를 추가하면 프로젝트 태스크의 전체 개수가 하나 증가한다`() {
         // given
+        val project = KanbanProjectState(name = "독도는 우리땅", toDoTask, inProgressTask, doneTask)
         val beforeProjectSize = project.totalCount
 
         // when
@@ -52,6 +32,8 @@ class KanbanProjectTest {
 
     @Test
     fun `TODO 상태의 태스크를 IN_PROGRESS로 변경하면 태스크의 상태가 IN_PROGRESS로 변경된다`() {
+        // given
+        val project = KanbanProjectState(name = "독도는 우리땅", toDoTask, inProgressTask, doneTask)
         val expectedTask = toDoTask.copy(status = Status.IN_PROGRESS)
 
         // when
@@ -63,6 +45,9 @@ class KanbanProjectTest {
 
     @Test
     fun `DONE 상태의 태스크를 가져오면 DONE 상태의 태스크만 반환된다`() {
+        // given
+        val project = KanbanProjectState(name = "독도는 우리땅", toDoTask, inProgressTask, doneTask)
+
         // when
         val doneTasks = project.getTasks(Status.DONE)
 
@@ -72,16 +57,28 @@ class KanbanProjectTest {
 
     @Test
     fun `3개의 태스크를 가진 프로젝트라면 전체 태스크 개수는 3개이다`() {
+        // given
+        val project = KanbanProjectState(name = "독도는 우리땅", toDoTask, inProgressTask, doneTask)
+
+        // then
         assertThat(project.totalCount).isEqualTo(3)
     }
 
     @Test
     fun `프로젝트의 DONE 상태의 태스크가 1개라면 완료한 개수도 1개이다`() {
+        // given
+        val project = KanbanProjectState(name = "독도는 우리땅", toDoTask, inProgressTask, doneTask)
+
+        // then
         assertThat(project.completeCount).isEqualTo(1)
     }
 
     @Test
     fun `3개의 태스크 중 한개만 DONE 상태라면 완료율의 소수 둘째자리까지의 값은 0_33에 가깝다`() {
+        // given
+        val project = KanbanProjectState(name = "독도는 우리땅", toDoTask, inProgressTask, doneTask)
+
+        // then
         assertThat(project.completeRatio).isCloseTo(0.33f, Offset.offset(0.01f))
     }
 
@@ -93,4 +90,33 @@ class KanbanProjectTest {
         // then
         assertThat(emptyProject.completeRatio).isEqualTo(0f)
     }
+
+    @Test
+    fun `review와 done 상태의 태스크를 삭제해도 제거되지 않는다`() {
+        // given
+        val project = KanbanProjectState(name = "독도는 우리땅", reviewTask, doneTask)
+
+        // when
+        project.deleteTask(reviewTask)
+        project.deleteTask(doneTask)
+
+        // then
+        assertThat(project.getTasks(Status.DONE)).contains(doneTask)
+        assertThat(project.getTasks(Status.REVIEW)).contains(reviewTask)
+    }
+
+    private val toDoTask: Task = Task(title = "우리땅!", tags = Tags(emptyList()), status = Status.TODO, user = User.Assignee("두릅"))
+    private val inProgressTask: Task = Task(
+        title = "우리땅!",
+        tags = Tags(emptyList()),
+        status = Status.IN_PROGRESS,
+        user = User.Assignee("부릅"),
+    )
+    private val doneTask: Task = Task(title = "우리땅!", tags = Tags(emptyList()), status = Status.DONE, user = User.Assignee("꽈뚜릅"))
+    private val reviewTask: Task = Task(
+        title = "Review Task",
+        tags = Tags(emptyList()),
+        status = Status.REVIEW,
+        user = User.Assignee("스마일")
+    )
 }
